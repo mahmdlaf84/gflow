5fc150cbd391657a626959abe8db11a3b0900d33
mahmdlaf84 <mahmdlaf@gmail.com>
2025-11-02 18:40:03 +0800

    Fix local network imports for env utilities

--- apps/sim/lib/urls/utils.ts
+++ apps/sim/lib/urls/utils.ts
@@ -1,8 +1,8 @@
 import { getEnv } from '@/env'
 import { isProd } from '@/environment'
-import { buildSystemUrl, getSystemIPAddress } from '@/network'
+import { buildSystemUrl, getSystemIPAddress } from '../network'
 
-export { buildSystemUrl, getSystemIPAddress } from '@/network'
+export { buildSystemUrl, getSystemIPAddress } from '../network'
 
 const LOCAL_HOSTNAME_PATTERNS = [
   'localhost',
